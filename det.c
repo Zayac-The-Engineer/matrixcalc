2b7dec2f783ebd1c5b6d6a46da775efdc12e466d
Zayac-The-Engineer <fed4chuck@gmail.com>
2020-09-22 01:46:32 +0300

    Initial commit

---
+++ det.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int det(const int**M, unsigned const int x);
+
+int det(const int**M, unsigned const int x)
+{
+	int i, j, k, d;
+	for(k=0; k<x; k++)
+	{
+		for(i=0, j=0; i<x ; i++, j=i)
+		{
+			
+		}
+	}
+	return d;
+}
+
+output(const int**M, unsigned const int x)
+{
+	int i, j;
+
+	for(i=0; i<x; i++)
+	{
+		for(j=0; j<x; j++)
+		{
+			printf("%d\t", M[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+int main()
+{
+	int x, i, j;
+	int**M;
+
+	printf("Sqare matrix dim: ");
+	scanf("%d", &x);
+
+	M = (int**)malloc(x*sizeof(int*));
+
+	for(i=0; i<x; i++)
+	{
+		M[i] = (int*)malloc(x*sizeof(int));
+	}
+
+	printf("Input matrix\n");
+	
+	for(i=0; i<x; i++)
+	{
+		for(j=0; j<x; j++)
+		{
+			scanf("%d", M[i][j]);
+		}
+	}
+
+	output(M, x);
+}
