fadd8378e1fdd2fb952f8dfcca6070feb8f38a43
Zayac-The-Engineer <fed4chuck@gmail.com>
2020-09-22 22:57:20 +0300

    Fuckin' works

--- det.c
+++ det.c
@@ -1,22 +1,63 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int det(const int**M, unsigned const int x);
+int det(int **M, unsigned int x);
+void output(int **M, unsigned int x);
 
-int det(const int**M, unsigned const int x)
+int det(int **M, unsigned int x)
 {
-	int i, j, k, d;
+	int *buf = (int *)calloc(x, sizeof(int));
+	int i, j, k;
+	int b = 1;
+	int d = 0;
+
 	for(k=0; k<x; k++)
 	{
-		for(i=0, j=0; i<x ; i++, j=i)
+		for(i=0; i<x; i++)
 		{
-			
+			for(j=0; j<x; j++)
+			{
+				if((i+k)%x==j)
+				{
+					buf[j] = M[i][j];
+				}
+			}
 		}
+
+		for(i=0; i<x; i++)
+		{
+			b *= buf[i];
+		}
+		d += b;
+		b = 1;
 	}
+
+	//I dunno, should probably work
+	for(k=0; k<x; k++)
+	{
+		for(i=0; i<x; i++)
+		{
+			for(j=x; j>0; j--)
+			{
+				if((i+k)%x==x-j)
+				{
+					buf[j] = M[i][j];
+				}
+			}
+		}
+
+		for(i=0; i<x; i++)
+		{
+			b *= buf[i];
+		}
+		d -= b;
+		b = 1;
+	}
+
 	return d;
 }
 
-void output(const int**M, unsigned const int x)
+void output(int **M, unsigned int x)
 {
 	int i, j;
 
@@ -33,16 +74,16 @@ void output(const int**M, unsigned const int x)
 int main()
 {
 	int x, i, j;
-	int**M;
+	int **M;
 
 	printf("Sqare matrix dim: ");
 	scanf("%d", &x);
 
-	M = (int**)malloc(x*sizeof(int*));
+	M = (int **)malloc(x*sizeof(int *));
 
 	for(i=0; i<x; i++)
 	{
-		M[i] = (int*)malloc(x*sizeof(int));
+		M[i] = (int *)malloc(x*sizeof(int));
 	}
 
 	printf("Input matrix\n");
@@ -55,5 +96,6 @@ int main()
 		}
 	}
 
-	output(M, x);
+	/* output(M, x); */
+	printf("det: %d\n", det(M, x));
 }
